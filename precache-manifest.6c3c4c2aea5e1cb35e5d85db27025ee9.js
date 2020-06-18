9c5919206d8d3bdb0dfd4075b72d945284553830
Bongani Maphiri <b.maphiri@gmail.com>
2020-06-18 22:13:54 +0200

    Updates

---
+++ precache-manifest.6c3c4c2aea5e1cb35e5d85db27025ee9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da4f1e77d543121cda89ce03bd90d1bb",
+    "url": "/Diboiza/RoboFriends/index.html"
+  },
+  {
+    "revision": "d4add6107f8fd2eb1c82",
+    "url": "/Diboiza/RoboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e35c678f7f2b4c582f83",
+    "url": "/Diboiza/RoboFriends/static/css/main.1d45cd49.chunk.css"
+  },
+  {
+    "revision": "d4add6107f8fd2eb1c82",
+    "url": "/Diboiza/RoboFriends/static/js/2.7ec5fd90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Diboiza/RoboFriends/static/js/2.7ec5fd90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e35c678f7f2b4c582f83",
+    "url": "/Diboiza/RoboFriends/static/js/main.6c1e126c.chunk.js"
+  },
+  {
+    "revision": "d35f7752646dbce97fb4",
+    "url": "/Diboiza/RoboFriends/static/js/runtime-main.61f95ff7.js"
+  }
+]);
